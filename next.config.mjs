b9eb949e31ae0848006c9352442981bccb5cc0b9
Courtney Ring <courtneyring3@gmail.com>
2024-03-23 15:24:42 -0400

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/crawford-county-historical-society',
+};
 
 export default nextConfig;
